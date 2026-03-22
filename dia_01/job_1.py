bf5bf51cd000f0dece4ba293edc07e49c899daf9
llvorbei91-droid <llvorbei91@gmail.com>
2026-03-22 17:27:18 +0100

    ejercicio 1 bien hecho

--- dia_01/job_1.py
+++ dia_01/job_1.py
@@ -1,1 +1,10 @@
-print(' No la puedo estar cagando tanto para no sabber lo que tengo que hacer')
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+#nume = [f"{p} impar" if p %2 == 0
+# else f"{p} par" for p in numeros] no funciona. tengo que busca que funcione
+
+for p in numeros:
+   if p %2 == 0:
+       print(f'{p} es par')
+   else:
+        print(f'{p} es impar')
