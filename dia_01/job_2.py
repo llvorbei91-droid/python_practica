6397bcbeaaa57cc7f1db0f65cf6f0d09e535be39
llvorbei91-droid <llvorbei91@gmail.com>
2026-03-23 05:39:48 +0100

    ejercicio 2 interactivo real

--- dia_01/job_2.py
+++ dia_01/job_2.py
@@ -5,4 +5,21 @@ def saluda (nombre):
 nombre = input ("Como te llamas?: ")
 
 saluda(nombre)
+#Una de las ideas queria hacer. mini IA with Chatgpt
+
+"""def saluda(nombre):
+    print(f"Hola {nombre}, que tal estas?")
+
+    respuesta = input("Que tal por Termux móvil, te gusta? ")
+
+    if respuesta.lower() == "genial":
+        print("Me alegro, yo también pienso lo mismo 😎")
+    elif respuesta.lower() == "no":
+        print("No pasa nada, poco a poco te gustará más")
+    else:
+        print("Gracias por responder")
+
+nombre = input("Como te llamas?: ")
+
+saluda(nombre)
 
