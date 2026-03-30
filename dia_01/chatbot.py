ad4c49932d0dee1f21b3054e08786a769d0319c5
llvorbei91-droid <llvorbei91@gmail.com>
2026-03-30 23:54:10 +0200

    Primer commit: chatbot + memoria + fragSave

--- dia_01/chatbot.py
+++ dia_01/chatbot.py
@@ -12,3 +12,23 @@ if gusto == "si":
     print("Entonces vamos por buen camino 🚀")
 else:
     print("No pasa nada, poco a poco 😎")
+huellas = input("Quieres dejar algo este años?")
+memoria["huellas"] = huellas
+if huellas == "si":
+    print("No te rinda aun queda mucho tiempos")
+else:
+   print("Te lo estas plantiando todavia?")
+reflecion = input("Tienes alguna refleccion inportante?")
+if reflecion == "si":
+   print("Es bueno reflecionar las cosas")
+else:
+   print("No te coma la cabezas con cosas fuera de tu crontor")
+memoria["reflecion"] = reflecion
+relacion = input("Que tal tus relaciones con las persona / cosa?")
+if relacion == "tengo varia":
+   print(" Eres toro")
+memoria["relacion"] = relacion
+futuro_cerca = input ("Como te ves un dentro de 3 meses?")
+memoria["futuro_cerca"] = futuro_cerca
+futuro_lejos = input ("a largo plazo?")
+memoria["futuro_lejos"] = futuro_lejos
